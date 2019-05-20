4581517de24ab311722fe2caa83c53894fdee171
Jerry Moua <jerrymoua@gmail.com>
2019-05-19 22:22:52 -0500

    updating freinds survey

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friendsList = [
+    {
+		name: "Winnie the Pooh",
+		photo: "https://pbs.twimg.com/profile_images/691863315853762561/ClkLQse3.jpg",
+		preferences: [5, 4, 5, 1, 1, 5, 1, 5, 5, 5]
+    },
+    {
+        name:"Ahmed",
+        photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjP_ad8z5bM0hr3ONXkaf8u6XFUgQfv0gcKJinS-FVhLb52JOo",
+        preferences:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        Name: "Stephen",
+        photo: "https://images.pexels.com/photos/333850/pexels-photo-333850.jpeg?h=350&auto=compress&cs=tinysrgb",
+        preferences: [3, 2, 3, 4, 5, 4, 3, 2, 1, 3]
+    },
+    {
+		name: "Elmo",
+		photo: "https://pbs.twimg.com/profile_images/454270645879070720/82lJeUWO.jpeg",
+		preferences: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+    {
+        name:"Hayley",
+        photo:"https://live.staticflickr.com/5252/5403292396_0804de9bcf_b.jpg",
+        preferences: [3, 2, 5, 4, 5, 1, 2, 5, 4, 1]
+    },  
+    {
+        name:"Jason",
+        photo:"https://pbs.twimg.com/media/DJWhERJVYAE4e2_.jpg",
+        preferences: [1, 2, 3, 4, 3, 2, 1, 3, 3, 4]
+    },
+    {
+        name:"Tyler",
+        photo:"https://i.dailymail.co.uk/i/pix/2013/08/29/article-2405475-1B8389EE000005DC-718_634x550.jpg",
+        preferences: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+];
+
+module.exports = friendsList;
